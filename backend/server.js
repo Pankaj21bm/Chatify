e8494473fec4d7055c9d73b3c5c4105e39379f34
Pankaj saini <sainipankaj969411@gmail.com>
2025-01-22 06:11:42 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -30,10 +30,11 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
+  console.log("haha i am currently in the production")
   console.log(path.join(__dirname1, "../frontend/build"))
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "../frontend/build/index.html")); 
+    res.sendFile(path.resolve(__dirname1, "../frontend/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
